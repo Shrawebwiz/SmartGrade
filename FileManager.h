8d92f7586ac64f0e297c14e687c792aabb955998
Shravani <132183348+Shrawebwiz@users.noreply.github.com>
2025-07-16 18:54:37 +0530

    initial commit

--- FileManager.h
+++ FileManager.h
@@ -1,4 +1,4 @@
-// FileManager.h
+
 
 #ifndef FILEMANAGER_H
 #define FILEMANAGER_H
@@ -14,33 +14,31 @@ private:
     string filename;
 
 public:
-    // Constructor to set the filename
+  
     FileManager(string fname);
 
-    // Add a new student record to the file
+
     void addStudent(const Student& s);
 
-    // View all student records from the file
+
     void viewAllStudents();
 
-    // Search and display a student by roll number
+ 
     bool searchStudent(int roll);
 
-    // Update student details by roll number
     void updateStudent(int roll);
 
-    // Delete a student record by roll number
+
     void deleteStudent(int roll);
 
-    // Flag and display students with marks below passing threshold (e.g., <40)
+
     void flagPerformance();
 
 private:
-    // Utility: Load all students from file into a vector
+
     vector<Student> loadAllStudents();
 
-    // Utility: Save all students from vector back to file (used in update/delete)
     void saveAllStudents(const vector<Student>& students);
 };
 
-#endif // FILEMANAGER_H
+#endif
